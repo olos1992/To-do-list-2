64c508fb88a810fbd48ee419dc9e4e5f22b16209
Aleksander Skula <aleksander.skula@gmail.com>
2024-02-28 05:56:24 +0100

    add new task functionality added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
 {
+    const tasks = [
+        {
+            content: "Zadanie 1",
+            done: true,
+        },
+        {
+            content: "Zadanie 2",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let listHtml = "";
+
+        for (task of tasks) {
+            listHtml +=
+                `<li ${task.done ? 'class="list__item--done"' : ""}>
+            ${task.content}
+            </li>`
+        };
+
+        document.querySelector(".js-tasksList").innerHTML = listHtml;
+
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({ content: newTaskContent });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+
     const init = () => {
+
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit)
 
     };
 
